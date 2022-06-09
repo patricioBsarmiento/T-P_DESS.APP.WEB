7358e7a04d4a37f26dfa22e526dc42c40a55f11f
Jazmin <jppinedac940@hotmail.com>
2022-06-09 20:20:35 -0300

    se agrega archivo de validaciones de javascript

---
+++ TP_desarrollo_de_app/js/validaciones.js
@@ -0,0 +1,127 @@
+function apellido(){
+    validacion=document.getElementById("apellido")
+    letras = validacion.value; 
+    parent = validacion.parentElement;
+    texto = document.createElement('DIV');
+    texto.setAttribute("id", "txtApellido")
+    texto.innerHTML='<span> Falta apellido </span>';
+    if (letras == null || letras == "" || /^[0-9]+$/.test(letras)){
+        validacion.setAttribute("class", "error")
+        if (document.getElementById("txtApellido") === null){
+            parent.appendChild(texto)
+        }
+        
+    }
+    else{
+        element = document.getElementById("txtApellido") 
+        if(element!== null){
+            parent.removeChild(element)
+        }
+        validacion.setAttribute("class", "click")
+    }       
+}
+
+function nombre(){
+    validacion=document.getElementById("nombre")
+    letras = validacion.value; 
+    parent = validacion.parentElement;
+    texto = document.createElement('DIV');
+    texto.setAttribute("id", "txtNombre")
+    texto.innerHTML='<span> Falta nombre </span>';
+    if (letras == null || letras == "" || /^[0-9]+$/.test(letras)){
+        validacion.setAttribute("class", "error")
+
+        if (document.getElementById("txtApellido") === null){
+            parent.appendChild(texto)
+        }
+    }
+    else{
+        element = document.getElementById("txtNombre") 
+        if(element!== null){
+            parent.removeChild(element)
+        }
+        validacion.setAttribute("class", "click")
+    }       
+}
+function tecla(id){
+    validacion=document.getElementById(id)
+    validacion.setAttribute("class", "onkeydown")
+}
+
+function validaEmail(){
+    email=document.getElementById("email")
+    emailConf=document.getElementById("email_conf")
+    valorEmail = email.value;
+    valorEmailConf = emailConf.value; 
+    parent = email.parentElement;
+    texto = document.createElement('DIV');
+    texto.setAttribute("id", "txtEmail")
+    texto.innerHTML='<span> Los correos electrónicos no son iguales </span>';
+    if (valorEmail !== valorEmailConf){
+        email.setAttribute("class", "error")
+
+        if (document.getElementById("txtEmail") === null){
+            parent.appendChild(texto)
+        }
+    }
+    else{
+        element = document.getElementById("txtEmail") 
+        if(element!== null){
+            parent.removeChild(element)
+        }
+        validacion.setAttribute("class", "click")
+    }
+    emailValido = document.createElement('DIV');
+    emailValido.setAttribute("id", "txtemailValido")
+    emailValido.innerHTML='<span> El correo no es válido, verifique que tenga @ y finalice en .com </span>';  
+    if (!email.checkValidity() && !(/\.[a-z]{3}$/.test(valorEmail)) ){
+        validacion.setAttribute("class", "error")
+
+        if (document.getElementById("txtemailValido") === null){
+            parent.appendChild(emailValido)
+        }
+    }   
+    else{
+        element = document.getElementById("txtemailValido") 
+        if(element!== null){
+            parent.removeChild(element)
+        }
+        validacion.setAttribute("class", "click")
+    }  
+}
+
+
+
+function mouse(id){
+    validacion=document.getElementById(id)
+    validacion.setAttribute("class", "onmouseover")   
+}
+function confirmacion(){
+    
+    validacion = document.getElementById("numero1");
+    validacion2 = document.getElementById("numero2");
+    letras = validacion.value;
+    letras2 = validacion2.value;
+    if (letras == null || letras == "" || /^[0-9]+$/.test(letras)){
+        validacion.setAttribute("class", "error")
+        alert("Escriba apellido correctamente")
+        
+    }
+    if (letras2 == null || letras2 == "" || /^[0-9]+$/.test(letras2)){
+        validacion2.setAttribute("class", "error")
+        alert("Escriba nombre correctamente")
+    }
+    validacion3=document.getElementById("numero3")
+    letras3=validacion3.value
+    if (/[A-Za-z]+$/.test(letras3)){
+        validacion3.setAttribute("class", "error")
+        alert("Escriba en numero el DNI")
+    }
+    validacion4=document.getElementById("numero4")
+    letras3=validacion4.value
+    if (letras3 != null || letras3 != "f" || letras3 != "m"|| letras3 == "" ){
+        validacion4.setAttribute("class", "error")
+        alert("Escriba f o m") 
+    }
+    
+}
